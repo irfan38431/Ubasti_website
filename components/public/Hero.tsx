8dd8ac4c0d76aa83310a50e0c5e5942fe58dbe76
irfan38-dzruptiv <irfan@dzruptiv.ai>
2026-05-18 15:44:29 +0530

    updated hero image

--- components/public/Hero.tsx
+++ components/public/Hero.tsx
@@ -20,7 +20,7 @@ export function Hero() {
           alt="Cozy interior of Ubasti Cat Cafe"
           fill
           className="object-cover"
-          style={{ objectPosition: "center 65%" }}
+          style={{ objectPosition: "center 72%" }}
           priority
           sizes="100vw"
         />
