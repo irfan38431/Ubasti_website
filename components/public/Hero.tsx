563e99c33540519f08caf40d0ca43e99c5d1635c
irfan38-dzruptiv <irfan@dzruptiv.ai>
2026-05-18 14:22:33 +0530

    herp-bannef

--- components/public/Hero.tsx
+++ components/public/Hero.tsx
@@ -20,7 +20,7 @@ export function Hero() {
           alt="Cozy interior of Ubasti Cat Cafe"
           fill
           className="object-cover"
-          style={{ objectPosition: "center 50%" }}
+          style={{ objectPosition: "center 65%" }}
           priority
           sizes="100vw"
         />
